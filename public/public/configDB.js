c66668bfa4c0c75072ed3b52eb3df8a77d621066
Trovoada <39368@alunos.isel.ipl.pt>
2015-07-31 22:22:03 +0100

    do it

--- public/public/configDB.js
+++ public/public/configDB.js
@@ -1,8 +1,9 @@
 module.exports= {
-                            hostname:"localhost",
-                            username:"postgres",
-                            password:"ruben10",
+                            hostname:"ec2-54-83-10-210.compute-1.amazonaws.com",
+                            username:"nlyrrwmhcfafsz",
+                            password:"MEGIvrN_VwhfgW-Ab7_xh1QlR9",
                             server:"",
-                            port:"3000",
-                            database:"ChelasLxDB"
+                            port:"5432",
+                            database:"d4akpqiqta03tf"
+							//postgres://nlyrrwmhcfafsz:MEGIvrN_VwhfgW-Ab7_xh1QlR9@ec2-54-83-10-210.compute-1.amazonaws.com:5432/d4akpqiqta03tf"
 };
