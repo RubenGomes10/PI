32616d54bf7d4d025854eda6f5825ec1da7f1a60
unknown <User01@49Y2K12.AGAP2.CORP>
2015-08-11 01:39:39 +0100

    up

--- models/encrypts.js
+++ models/encrypts.js
@@ -3,17 +3,24 @@
  */
 var bcrypt = require('bcryptjs');
 
-module.exports.hash =  function(text, callback){
-    bcrypt.genSalt(10,function(err,salt){
-        if(err){callback(err); return ;}
-        bcrypt.hash(text,salt,function(err,hash){
-            callback(err,hash);
+var encrypt = {
+    hash: function (text, callback) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            bcrypt.hash(text, salt, function (err, hash) {
+                callback(err, hash);
+            });
         });
-    });
-};
+    },
 
-module.exports.compare = function(text1,text2,callback){
-    bcrypt.compare(text1,text2,function(err,res){
-        callback(err,res);
-    });
-};
+    compare: function (text1, text2, callback) {
+        bcrypt.compare(text1, text2, function (err, res) {
+            callback(err, res);
+        });
+    }
+}
+
+module.exports = encrypt;
